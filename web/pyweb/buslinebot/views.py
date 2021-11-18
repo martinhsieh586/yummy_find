2e97c17649cbaf0679082bace526a3c3706d2939
martin <martinhsieh586@gmail.com>
2021-11-18 17:55:51 +0800

    make it better

--- web/pyweb/buslinebot/views.py
+++ web/pyweb/buslinebot/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
